dfa17017935de8e92cb6f0b79a3c0520f741a80f
Seiya Miyazaki <smsmiyazaki@gmail.com>
2023-01-25 18:27:27 -0500

    Commenting and organizing

--- _tests_/Intern.test.js
+++ _tests_/Intern.test.js
@@ -1,11 +1,19 @@
 const Intern = require('../lib/Intern');
 
-test('creates an intern object', () => {
-    const intern = new Intern('John Doe', 1, 'johndoe@gmail.com', 'UCLA');
+describe('Intern', () => {
+  let intern;
 
-    expect(intern.name).toBe('John Doe');
-    expect(intern.id).toBe(1);
-    expect(intern.email).toBe('johndoe@gmail.com');
-    expect(intern.school).toBe('UCLA');
+
+  beforeEach(() => { // Create a new instance of Intern before each test
+    intern = new Intern('John Doe', 1, 'johndoe@gmail.com', 'UCLA');
+  });
+
+  
+  test('creates an instance of Intern', () => {// Test that the Intern class creates an instance with the correct properties and values
+    expect(intern.getName()).toBe('John Doe');
+    expect(intern.getId()).toBe(1);
+    expect(intern.getEmail()).toBe('johndoe@gmail.com');
+    expect(intern.getSchool()).toBe('UCLA');
     expect(intern.getRole()).toBe('Intern');
+  });
 });
--- _tests_/Manager.test.js
+++ _tests_/Manager.test.js
@@ -1,6 +1,6 @@
 const Manager = require("../lib/Manager");
 
-describe('Manager', () => {
+describe('Manager', () => { // Test that the officeNumber can be set via the constructor argument
   it('Can set office number via constructor argument', () => {
     const testValue = 100;
     const e = new Manager('Foo', 1, 'test@test.com', testValue);
@@ -8,30 +8,33 @@ describe('Manager', () => {
   });
 
   it('getRole() should return "Manager"', () => {
+    // Test that the getRole method returns "Manager"
     const testValue = "Manager";
     const e = new Manager('Foo', 1, 'test@test.com', 100);
     expect(e.getRole()).toBe(testValue);
   });
 
-  it('Can get name via getName()', () => {
+  it('Can get name via getName()', () => {// Test that the getName method returns the correct name
     const testValue = 'Foo';
     const e = new Manager(testValue, 1, 'test@test.com', 100);
     expect(e.getName()).toBe(testValue);
   });
 
-  it('Can get id via getId()', () => {
+  it('Can get id via getId()', () => {  // Test that the getId method returns the correct id
     const testValue = 1;
     const e = new Manager('Foo', testValue, 'test@test.com', 100);
     expect(e.getId()).toBe(testValue);
   });
 
   it('Can get email via getEmail()', () => {
+    // Test that the getEmail method returns the correct email
     const testValue = 'test@test.com';
     const e = new Manager('Foo', 1, testValue, 100);
     expect(e.getEmail()).toBe(testValue);
   });
 
-  it('Can get office number via getOfficeNumber()', () => {
+  it('Can get office number via getOfficeNumber()', () => {  // Test that the getOfficeNumber method returns the correct officeNumber
+
     const testValue = 100;
     const e = new Manager("Foo", 1, "test@test.com", testValue);
     expect(e.getOfficeNumber()).toBe(testValue);
